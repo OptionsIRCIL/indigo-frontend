f2f60adfea5210e84c9e84c995de31d31b2465c0
Daisy <Daisy.dpr@gmail.com>
2025-12-16 15:41:41 -0600

    Main page / Search Page UI
    Minimal / no CSS. Basic UI of main page/ search page.

--- src/app/main-dashboard/main-dashboard.component.ts
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -1,18 +1,56 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from "@angular/material/button";
+import { MatFormField } from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
 import { AccessClientService } from "../../service/client/access-client.service";
+import { NgFor } from '@angular/common';
+import { last } from 'rxjs';
 
 @Component({
 	selector: "app-main-dashboard",
-	imports: [MatButtonModule],
+	imports: [
+		MatButtonModule,
+		MatFormField,
+		MatInputModule,
+		MatIconModule,
+		MatChipsModule,
+		MatExpansionModule,
+		MatCardModule,
+		NgFor
+	],
 	templateUrl: "./main-dashboard.component.html",
 	styleUrl: "./main-dashboard.component.css",
 })
 export class MainDashboardComponent {
 	public constructor(private readonly accessClient: AccessClientService) {
 	}
+	selectedFilters: string[] = ["State - ND", "City - Grand Forks", "Test"];
 
 	protected logout() {
 		this.accessClient.logout();
+	}
+
+	openFilters() {
+		console.log("Filters opened");
+	}
+
+	removeFilter(filter: string) {
+		this.selectedFilters = this.selectedFilters.filter(f => f !== filter);
+	}
+
+	search() {
+		console.log("Searching...");
+	}
+
+	addNewPerson() {
+		console.log("Adding new person...");
+	}
+
+	organizationResults() {
+		console.log("Showing organization results...");
 	}
 }
