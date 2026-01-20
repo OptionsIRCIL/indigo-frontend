99fa301dca66d019572f96b8db775937a917142e
AngelinaToste <amtoste9@gmail.com>
2026-01-20 10:41:26 -0500

    add person/ add individual rough draft progress
    added more to add individual contact information and disability info form.

--- src/app/main-dashboard/add-person/add-person.component.ts
+++ src/app/main-dashboard/add-person/add-person.component.ts
@@ -3,6 +3,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Injectable({ providedIn: 'root' })
 export class AddPersonDialogService {
@@ -77,11 +82,16 @@ export class AddPersonContentDialog {
 @Component({
   selector: 'individual-content-dialog',
   templateUrl: 'individual-content.html',
+  styleUrls: ['individual-content.css'],
   imports: [
     MatDialogModule,
     MatIconModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
   ]
 })
 export class IndividualContentDialog {
