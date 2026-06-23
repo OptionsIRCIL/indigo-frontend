0a48a6a37716c09f91eaa1b4795b5064c1daeba2
Kyle Holter Vogel <44628526+kylehovo@users.noreply.github.com>
2026-06-23 15:04:41 -0500

    Remove old form selector

--- src/app/view-record/view-record.component.ts
+++ src/app/view-record/view-record.component.ts
@@ -18,10 +18,7 @@ import { MatDialog } from "@angular/material/dialog";
 import { Router, ActivatedRoute } from "@angular/router";
 import { TokenState } from "../../service/state/token-state.service";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import {
-	AddFormContentDialog,
-	AddAttachmentContentDialog,
-} from "./add-form-or-attachment/add-form-or-attachment.component";
+import { AddAttachmentContentDialog } from "./add-form-or-attachment/add-form-or-attachment.component";
 import { OpenFormsService } from "./open-forms.service";
 import { ConfigService } from "../../config/config.service";
 import { PersonClientService } from "../../service/client/person-client.service";
